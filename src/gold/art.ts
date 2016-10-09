7fb951d67287f5f89b161cc4c674600b3575c194
Tom Palmer <tjpalmer@tjpalmer.com>
2016-10-08 19:19:50 -0700

    Texture from image

--- src/gold/art.ts
+++ src/gold/art.ts
@@ -1,9 +1,16 @@
+import {NearestFilter, Texture} from 'three';
+
 export class Art {
 
   constructor() {
     let image = new Image();
     image.src = require('./blocks.png');
+    this.texture = new Texture(image);
+    this.texture.magFilter = NearestFilter;
+    this.texture.needsUpdate = true;
   }
+
+  texture: Texture;
 
 }
 
