1d9e39b4b228819fa0ebb66c00e2f8d302bdee8c
Tom <tjpalmer@tjpalmer.com>
2016-10-10 21:26:20 -0700

    Display brick tiles

--- src/gold/art.ts
+++ src/gold/art.ts
@@ -1,6 +1,7 @@
 import {Level, Stage} from '../';
 import {
-  Mesh, MeshBasicMaterial, NearestFilter, PlaneBufferGeometry, Texture
+  Mesh, MeshBasicMaterial, NearestFilter, PlaneBufferGeometry, ShaderMaterial,
+  Texture, Vector2
 } from 'three';
 
 export class Art {
@@ -15,6 +16,7 @@ export class Art {
   }
 
   handle(stage: Stage) {
+    // Background test.
     let plane =
       new PlaneBufferGeometry(Level.pixelCount.x, Level.pixelCount.y, 1, 1);
     let planeMaterial = new MeshBasicMaterial({
@@ -23,6 +25,26 @@ export class Art {
     let mesh = new Mesh(plane, planeMaterial);
     mesh.position.set(Level.pixelCount.x / 2, Level.pixelCount.y / 2, 0);
     stage.scene.add(mesh);
+    // Tile test.
+    let tilePlane =
+      new PlaneBufferGeometry(Level.tileSize.x, Level.tileSize.y, 1, 1);
+    let tileMaterial = new ShaderMaterial({
+      uniforms: {
+        map: {value: this.texture},
+      },
+      fragmentShader: tileFragmentShader,
+      vertexShader: tileVertexShader,
+    });
+    let offset = new Vector2();
+    for (let j = 0; j < Level.tileCount.x; ++j) {
+      for (let i = 0; i < Level.tileCount.y; ++i) {
+        let tileMesh = new Mesh(tilePlane, tileMaterial);
+        offset.set(j + 0.5, i + 0.5).multiply(Level.tileSize);
+        tileMesh.position.set(offset.x, offset.y, 0);
+        stage.scene.add(tileMesh);
+        break;
+      }
+    }
     // Render.
     stage.render();
   }
@@ -42,3 +64,26 @@ export class Art {
 }
 
 declare function require(name: string): any;
+
+let tileFragmentShader = `
+  uniform sampler2D map;
+  varying vec2 vUv;
+  void main() {
+    vec2 coord = (
+      (vUv + vec2(0, 0)) * vec2(8, 10) + vec2(0, 56)
+    ) / vec2(512, 256);
+    gl_FragColor = texture2D(map, coord);
+    // gl_FragColor.w = gl_FragColor.x * gl_FragColor.y * gl_FragColor.z;
+    // if (gl_FragColor.w > 0.0) {
+    //   gl_FragColor.w = 1.0;
+    // }
+  }
+`;
+
+let tileVertexShader = `
+  varying vec2 vUv;
+  void main() {
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    vUv = uv;
+  }
+`;
