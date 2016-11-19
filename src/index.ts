6db6113375db1f1b650b7e0a24271c3ef207cd23
Tom <tjpalmer@tjpalmer.com>
2016-11-19 13:25:15 -0700

    Separate part from stage

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@
 
 // Some others depend on Mode and Part from these for now, so import it first.
 export * from './game';
+export * from './part';
 export * from './stage';
 
 export * from './control';
--- src/stage.ts
+++ src/stage.ts
@@ -1,58 +1,5 @@
-import {Level, Game, Grid} from './';
+import {Game, Grid, Level, Part} from './';
 import {Vector2} from 'three';
-
-export enum Edge {
-  top,
-  right,
-  bottom,
-  left,
-}
-
-export class Part {
-
-  constructor(game: Game) {
-    this.game = game;
-  }
-
-  art: any = undefined;
-
-  // Bars catch heros and enemies, and burned bricks catch enemies (or inside
-  // solid for burned bricks?).
-  catches(part: Part) {
-    return false;
-  }
-
-  climbable = false;
-
-  contains(point: Vector2) {
-    let {x, y} = this.point;
-    return (
-      point.x >= x && point.x < x + Level.tileSize.x &&
-      point.y >= y && point.y < y + Level.tileSize.y);
-  }
-
-  // For overriding.
-  editPlacedAt(tilePoint: Vector2) {}
-
-  game: Game;
-
-  point = new Vector2();
-
-  // TODO Inside solid for burned bricks vs enemies, or launchers for all?
-  solid(edge?: Edge) {
-    return false;
-  }
-
-  surface = false;
-
-  tick() {}
-
-}
-
-export interface PartType {
-  new (game: Game): Part;
-  char: string;
-}
 
 export class Stage {
 
