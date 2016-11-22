4f66fd4867f44a837affbdf6ee0be562badc77a0
Tom Palmer <tjpalmer@tjpalmer.com>
2016-11-21 22:18:13 -0700

    A bit cleaner

--- src/parts/runner.ts
+++ src/parts/runner.ts
@@ -33,16 +33,12 @@ export class Runner extends Part {
 
   getClimbable(leftParts: Array<Part>, rightParts: Array<Part>) {
     let isClimbable = (part: Part) => part.climbable && part != this;
-    return (
-      leftParts.find(isClimbable) ||
-      rightParts.find(isClimbable));
+    return leftParts.find(isClimbable) || rightParts.find(isClimbable);
   }
 
   getSurface(leftParts: Array<Part>, rightParts: Array<Part>) {
     let isSurface = (part: Part) => part.surface && part != this;
-    return (
-      leftParts.find(isSurface) ||
-      rightParts.find(isSurface));
+    return leftParts.find(isSurface) || rightParts.find(isSurface);
   }
 
   // TODO Switch to using this once we have moving supports (enemies).
