877c2d7ce464452ed3e83089f1495c9156d71798
Tom <tjpalmer@tjpalmer.com>
2016-12-17 20:12:25 -0700

    Treasures, even gatherable somewhat

---
+++ src/gold/treasure.ts
@@ -0,0 +1,23 @@
+import {Art, Layer} from './';
+import {Treasure} from '../parts';
+import {Vector2} from 'three';
+
+export class TreasureArt implements Art {
+
+  constructor(treasure: Treasure) {
+    this.treasure = treasure;
+  }
+
+  layer = Layer.treasure;
+
+  get tile() {
+    return this.treasure.owner ? goneTile : mainTile;
+  }
+
+  treasure: Treasure;
+
+}
+
+let goneTile = new Vector2(0, 2);
+
+let mainTile = new Vector2(13, 17);
